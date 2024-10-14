d5fa204f8138e2a10cf7da2aa23e402a311e0725
Arthur Kramberger <arthur1curi@gmail.com>
2024-10-14 16:35:42 -0300

    Payment, one-to-one association

--- src/main/java/com/udemycourse/springcourse/config/TestConfig.java
+++ src/main/java/com/udemycourse/springcourse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.udemycourse.springcourse.entities.Category;
 import com.udemycourse.springcourse.entities.Order;
 import com.udemycourse.springcourse.entities.OrderItem;
+import com.udemycourse.springcourse.entities.Payment;
 import com.udemycourse.springcourse.entities.Product;
 import com.udemycourse.springcourse.entities.User;
 import com.udemycourse.springcourse.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
-		
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }
